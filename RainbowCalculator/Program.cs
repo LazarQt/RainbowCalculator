f641d442f41df59d10767574f9df10b76c19a91a
Lazar Gosic <lazar.gosic@gmail.com>
2022-04-07 11:07:31 +0200

    test2

--- RainbowCalculator/Program.cs
+++ RainbowCalculator/Program.cs
@@ -1,14 +1,8 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddSingleton<ITestProvider, TestProvider>();
 
 var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.MapGet("/api/user/{userId}", (string userId, [FromServices] ITestProvider testProvider) => testProvider.Get(userId));
 app.Run();
