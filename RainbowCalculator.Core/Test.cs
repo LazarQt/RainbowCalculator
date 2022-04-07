238f11ef65dd63f71d0039e726cc4180743dee3c
Lazar Gosic <lazar.gosic@gmail.com>
2022-04-07 10:16:36 +0200

    console app added

---
+++ RainbowCalculator.Core/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainbowCalculator.Core
+{
+    public static class Test
+    {
+        public static string GetThis() => "asdf";
+    }
+}
--- RainbowCalculator/Program.cs
+++ RainbowCalculator/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using RainbowCalculator.Core;
+
+var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.MapGet("/api/user", () => new { id = 1, name = "John Doe" });
+app.MapGet("/api/user", () => new { id = 1, name = Test.GetThis() });
 app.Run();
+
