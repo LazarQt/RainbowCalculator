a9994f312da0615f517e612f476295a26228923a
Lazar Gosic <lazar.gosic@gmail.com>
2022-04-08 14:53:29 +0200

    f

--- RainbowCalculator/ManabaseProvider.cs
+++ RainbowCalculator/ManabaseProvider.cs
@@ -9,32 +9,32 @@ namespace RainbowCalculator
         {
             List<Category> categories = new List<Category>();
             var categoryLines = CsvUtil.ReadLines(@"Categories.csv");
-            //foreach(var line in categoryLines)
-            //{
-            //    categories.Add(new Category()
-            //    {
-            //        Cycle = line[0],
-            //        Enters = line[1],
-            //        Order = Convert.ToDouble(line[2]),
-            //        Cutoff = Convert.ToInt32(line[3])
-            //    });
-            //}
+            foreach (var line in categoryLines)
+            {
+                categories.Add(new Category()
+                {
+                    Cycle = line[0],
+                    Enters = line[1],
+                    Order = Convert.ToDouble(line[2]),
+                    Cutoff = Convert.ToInt32(line[3])
+                });
+            }
 
-            //List<Model.Land> lands = new List<Model.Land>();
-            //var landLines = CsvUtil.ReadLines(@"Lands_Filtered.csv");
-            //foreach (var line in landLines)
-            //{
-            //    var category = categories.FirstOrDefault(c => c.Cycle == line[4]);
-            //    if (category == null) throw new Exception("unknown land category");
-            //    lands.Add(new Model.Land()
-            //    {
-            //        Name = line[0],
-            //        Identity = line[1],
-            //        Produces = line[2],
-            //        Order = category.Order,
-            //        Cutoff = category.Cutoff
-            //    });
-            //}
+            List<Land> lands = new List<Land>();
+            var landLines = CsvUtil.ReadLines(@"Lands_Filtered.csv");
+            foreach (var line in landLines)
+            {
+                var category = categories.FirstOrDefault(c => c.Cycle == line[4]);
+                if (category == null) throw new Exception("unknown land category");
+                lands.Add(new Land()
+                {
+                    Name = line[0],
+                    Identity = line[1],
+                    Produces = line[2],
+                    Order = category.Order,
+                    Cutoff = category.Cutoff
+                });
+            }
 
             return new[] { new LandSuggestion() { Name = "my name is" + deckString.Substring(0,1) } };
         }
