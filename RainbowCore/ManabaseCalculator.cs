2d7ca3ca554bba68706770de75bde4fb3b12fca6
Lazar Gosic <lazar.gosic@gmail.com>
2022-08-10 15:34:07 +0200

    heroku trigger

--- RainbowCore/ManabaseCalculator.cs
+++ RainbowCore/ManabaseCalculator.cs
@@ -16,7 +16,7 @@ namespace RainbowCore
 
         public LandSuggestion[] Calculate(string deckString)
         {
-            //return new LandSuggestion[] {new LandSuggestion(){Name = "why doesn thits how upp"} };
+            return new LandSuggestion[] {new LandSuggestion(){Name = "why doesn thits how upp3"} };
 
             var reader = new CalculationFilesReader();
 
